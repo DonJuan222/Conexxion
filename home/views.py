17d7085e77b1f2f4128620eb2157e8ca5106d67c
Juan <juan.polindara@misena.edu.co>
2022-10-31 10:15:28 -0500

    Registro 2.0

---
+++ home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                return HttpResponse('Usuario creado')
+            except:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    "error": 'El usuario ya existe'
+                })
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            "error": 'Contraseña no coinciden'
+        })
